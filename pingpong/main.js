97e4e4670816d0b1190deef266339edeca18900e
Jarre Knockaert <jarknock@ugent.be>
2017-05-27 10:07:24 +0200

    make the game object oriented

---
+++ pingpong/main.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    prepareGame();
+    frameLoop();
+    gameLoop();
+    respondToInput();
+}
--- pingpong/pingpong.js
+++ pingpong/pingpong.js
@@ -1,117 +1,156 @@
 /**
  * Created by Jarre on 26-5-2017.
  */
-
 //Global variables
-let canvas
-let context
-let boardWidth;
-let boardHeight;
-let player1;
-let player2;
-let ball;
-let frameInterval = 100
-let gameInterval = 60
-let finished = false
+const frameInterval = 100;
+const gameInterval = 60;
+const direction = {
+    UP: {x: 0, y: -1},
+    DOWN: {x: 0, y: 1},
+    LEFT: {x: -1, y: 0},
+    RIGHT: {x: 1, y: 0}
+}
+let game;
+let renderHandler;
 
-window.onload = function() {
-    prepareGame()
-    frameLoop()
-    gameLoop()
-    respondToInput()
+window.onload = function () {
+    renderHandler = new RenderHandler();
+    game = new Game();
+    game.createEntities();
+    game.frameLoop();
+    game.gameLoop();
+    game.respondToInput();
 }
 
-frameLoop = function(){
-    setInterval(function () {
-        drawEntities()
-    }, 1000/frameInterval)
+class Game {
+    constructor(){
+        this.canvas = document.getElementById('gameCanvas');
+        this.context = this.canvas.getContext('2d');
+        this.width = this.canvas.width
+        this.height = this.canvas.height
+    }
+
+    createEntities(){
+        let playerOffsetX = 40;
+        this.player1 = new Player(playerOffsetX)
+        this.player2 = new Player(this.width - playerOffsetX)
+        this.ball = new Ball()
+    }
+
+    frameLoop() {
+        let game = this
+        setInterval(function () {
+            game.context.beginPath();
+            game.draw()
+            game.player1.draw()
+            game.player2.draw()
+            game.ball.draw()
+
+        }, 1000/frameInterval)
+    }
+
+    gameLoop() {
+        let game = this
+        setInterval(function() {
+            game.ball.position.x += game.ball.speed;
+        }, 1000/gameInterval)
+    }
+
+    respondToInput() {
+        let game = this
+        $(document).keydown(function(e){
+            switch(e.keyCode){
+                case 38: //Up
+                    game.player1.move(direction.UP)
+                    break;
+                case 40: //Down
+                    game.player1.move(direction.DOWN)
+                    break;
+            }
+        })
+    }
+
+    draw() {
+        renderHandler.drawRectangle(0, 0, this.width, this.height, 'black');
+    }
+
+    //Get the center of the y-axis given the objects height.
+    getCenterY(height) {
+        return this.height/2 - height/2;
+    }
+
+    //Get the center of the x-axis given the objects height.
+    getCenterX(width) {
+        return this.width/2 - width/2;
+    }
+
 }
 
-gameLoop = function(){
-    setInterval(function() {
-        ball.position.x += ball.speed
-    }, 1000/gameInterval)
+//A drawable entity
+class Entity {
+    /**
+     * Create a new drawable entity
+     * @param position The coordinate of the entity.
+     * @param physicalProperties The physical properties of the entity such as width and height.
+     * @param speed: The speed at which the entity can move.
+     */
+    constructor(position, physicalProperties, speed){
+        this.position = position
+        this.physicalProperties = physicalProperties
+        this.speed = speed
+    }
 }
 
-respondToInput = function() {
-    $(document).keydown(function(e){
-        switch(e.keyCode){
-            case 38: //Up
-                player1.position.y -= player1.speed
-                break;
-            case 40: //Down
-                player1.position.y += player1.speed
-                break;
-        }
-    })
+class Player extends Entity {
+    constructor(x){
+        const speed = 6;
+        const width = 15;
+        const height = 80;
+        const color = 'white'
+        super({x, y: game.getCenterY(height)}, {width, height, color}, speed);
+    }
+
+    draw(){
+        renderHandler.drawRectangle(this.position.x, this.position.y,
+            this.physicalProperties.width, this.physicalProperties.height, this.physicalProperties.color);
+    }
+
+    move(direction){
+        this.position.y += this.speed * direction.y
+    }
 }
 
-prepareGame = function(){
-    createCanvas()
-    createPlayers()
-    ball = createBall()
+class Ball extends Entity {
+    constructor(){
+        const speed = 3;
+        const color = 'white';
+        const radius = 12;
+        super({x: game.getCenterX(radius), y: game.getCenterY(radius)}, {radius, color}, speed);
+    }
+
+    draw(){
+        renderHandler.drawCircle(this.position.x, this.position.y, this.physicalProperties.radius, this.physicalProperties.color);
+    }
+
+    move(directionX, directionY){
+        this.position.x += directionX.x
+        this.position.y += directionY.y
+    }
 }
 
-createCanvas = function(){
-    canvas = document.getElementById('gameCanvas')
-    context = canvas.getContext('2d')
-    boardWidth = canvas.width
-    boardHeight = canvas.height
+class RenderHandler {
+    drawRectangle(x, y, width, height, color) {
+        game.context.fillStyle = color;
+        game.context.fillRect(x, y, width, height);
+    }
+
+    drawCircle(x, y, radius, color) {
+        game.context.fillStyle = color;
+        game.context.arc(x, y, radius, 0, 2*Math.PI);
+        game.context.fill();
+    }
 }
 
-
-createPlayers = function(){
-    let speed = 6
-    let playerHeight = 80
-    let playerWidth = 15
-    let playerOffsetX = 40
-    let playerOffsetY = getCenterY(playerHeight)
-    player1 = createPlayer(playerOffsetX, playerOffsetY, playerWidth, playerHeight, speed)
-    player2 = createPlayer(boardWidth-playerOffsetX, playerOffsetY, playerWidth, playerHeight, speed)
-}
-
-createPlayer = function(x, y, width, height, speed){
-    let color = 'white'
-    return {position: {x, y}, width, height, color, speed}
-}
-
-createBall = function(){
-    let speed = 3
-    let color = 'white'
-    let radius = 12
-    let x = getCenterX(radius)
-    let y = getCenterY(radius)
-    return {position: {x, y}, radius, color, speed}
-}
-
-drawRectangle = function(x, y, width, height, color){
-    context.fillStyle = color
-    context.fillRect(x, y, width, height)
-}
-
-drawCircle = function(x, y, radius, color){
-    context.fillStyle = color
-    context.arc(x, y, radius, 0, 2*Math.PI)
-    context.fill()
-}
-
-//Get the center of the y-axis given the objects height.
-getCenterY = function(height){
-    return boardHeight/2 - height/2
-}
-
-//Get the center of the x-axis given the objects height.
-getCenterX = function(width){
-    return boardWidth/2 - width/2
-}
-
-drawEntities = function(){
-    context.beginPath()
-    drawRectangle(0, 0, boardWidth, boardHeight, 'black')
-    drawRectangle(player1.position.x, player1.position.y, player1.width, player1.height, player1.color)
-    drawRectangle(player2.position.x, player2.position.y, player2.width, player2.height, player2.color)
-    drawCircle(ball.position.x, ball.position.y, ball.radius, ball.color)
-}
 
 
 
